3d95f4ffb37984eadca6414ab384707ee12289ab
Taiwo <nafiutaiwosolomon@gmail.com>
2022-04-09 01:02:19 +0100

    First testing commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let Cards = [1, 2, 3, 4, 5, 6, 7 , 8, 9, ]
+let playText = document.getElementById('want-to-play')
+let cardEl = document.getElementById('cards-el')
+let sumEl = document.getElementById('sum-el')
+
+function startGame() {
+    console.log('start game')
+}
+function randomCard() [
+    return Math.floor(Math.random() * 13) + 1;
+]
+function newCard() {
+    console.log('new card')
+}
