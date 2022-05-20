8e9eaa72f9ad5ac7e463e637650ac6476484b20e
taiwonaf <nafiutaiwosolomon@gmail.com>
2022-05-20 04:19:43 +0100

    js included and css modified and completed

--- index.js
+++ index.js
@@ -1,14 +1,53 @@
-let Cards = [1, 2, 3, 4, 5, 6, 7 , 8, 9, ]
-let playText = document.getElementById('want-to-play')
-let cardEl = document.getElementById('cards-el')
-let sumEl = document.getElementById('sum-el')
+let messageEl = document.getElementById("message");
+let cardsEl = document.getElementById("cards-el");
+let sumEl = document.getElementById("sum-el");
+let newCard = document.getElementById("new");
+let startGame = document.getElementById("start");
+let blackjack = false;
+let isAlive = false;
+let cards =[]
+let sum = 0;
 
-function startGame() {
-    console.log('start game')
+function getRandomNumber() {
+    let randomNumber = Math.floor(Math.random()*13) + 1;
+    return randomNumber;
 }
-function randomCard() [
-    return Math.floor(Math.random() * 13) + 1;
-]
-function newCard() {
-    console.log('new card')
+
+startGame.onclick = () => {
+    newCard.style.display = "block";
+    isAlive = true;
+    let firstCard = getRandomNumber();
+    let secondCard = getRandomNumber();
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = `cards: `
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += ` ${cards[i]} `
+    }
+    sumEl.textContent = `sum: ${sum}`
+    if (sum < 21) {
+        messageEl.textContent = "Less than 21, play new card"
+    } else if ( sum === 21) {
+        messageEl.textContent = "You got a blackjack"
+        blackjack = true
+    } else {
+        messageEl.textContent = "Greater than 21, you lost"
+        newCard.style.display = "none"
+        isAlive = false
+    }
+}
+
+
+
+newCard.onclick = () => {
+    if (isAlive && !blackjack ) {
+        let newCard = getRandomNumber()
+        sum += newCard
+        cards.push(newCard)
+        renderGame()
+    }
 }
